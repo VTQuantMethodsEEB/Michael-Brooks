55500b7e06435bb703fa387c8c2aceb866183e0b
mrebrooks <mrebrooks@vt.edu>
2022-01-31 23:09:58 -0500

    Finished assignment 2 code and updated readme

--- Assignment 2_MRB.R
+++ Assignment 2_MRB.R
@@ -1,40 +1,58 @@
 rm(list=ls()) # clears workspace
 
 
-#load necessary packages
+#loads necessary packages
 library(tidyverse)
 library(reshape2)
 library(tidyr)
 
-Campylobacter <- read.csv("campylobacterreview_data.csv") #reads csv file from my meta-analysis data into workspace
-head(Campylobacter) #first 6 rows
-hist(Campylobacter$mass, breaks=150) #histogram of weights
+campylobacter <- read.csv("campylobacterreview_data.csv") #reads csv file from my meta-analysis data into workspace
+head(campylobacter) #first 6 rows
+hist(campylobacter$mass, breaks=150) #histogram of weights
 
-Campylobacter$cjejuni <- as.factor(Campylobacter$cjejuni) #force cjejuni variable into a factor
-levels(Campylobacter$cjejuni) #levels of cjejuni variable
+unique(campylobacter$cjejuni) #unique values for cjejuni variable (0=absence, 1=presence, NA=no data)
+unique(campylobacter$family) #unique values for family variable
+str (campylobacter$mass) #structure of the mass variable (numeric, continuous)
 
-Campylobacter$ccoli <- as.factor(Campylobacter$ccoli) #force ccoli variable into a factor
-levels(Campylobacter$ccoli) #levels of ccoli variable
+campylobacter$cjejuni <- as.factor(campylobacter$cjejuni) #force cjejuni variable into a factor
+str(Campylobacter$cjejuni) #structure of cjejuni variable
 
-Campylobacter$campylobacter <- as.factor(Campylobacter$campylobacter) #force campylobacter variable into a factor
-levels(Campylobacter$campylobacter) #levels of campylobacter variable
+campylobacter$ccoli <- as.factor(campylobacter$ccoli) #force ccoli variable into a factor
+str(Campylobacter$ccoli) #structure of ccoli variable
 
-view(Campylobacter) #pulls up Campylobacter table
+campylobacter$campy <- as.factor(campylobacter$campylobacter) #force campylobacter variable into a factor
+str(Campylobacter$campylobacter) #structure of campylobacter variable
+
+view(campylobacter) #pulls up Campylobacter table
 
 #group by campylobacter and mean mass (in grams)
-Campylobacter %>%
-  group_by(campylobacter) %>%
+campylobacter %>%
+  group_by(campy) %>%
   summarise(mean.mass=mean(mass, na.rm=TRUE))
 
 #group by cjejuni and mean mass (in grams)
-Campylobacter %>%
+campylobacter %>%
   group_by(cjejuni) %>%
   summarise(mean.mass=mean(mass, na.rm=TRUE))
 
-#group by ccoli and mean mass (in grams)
-Campylobacter %>%
-  group_by(ccoli) %>%
+#group by trophic and mean mass (in grams)
+campylobacter %>%
+  group_by(trophic) %>%
   summarise(mean.mass=mean(mass, na.rm=TRUE))
+
+#adds a new column mean.mass.campy to a new tibble, groups mean mass by campy infection status
+campylobacter_mutated = campylobacter %>%
+  group_by(campy) %>%
+  mutate(mean.mass.campy=mean(mass, na.rm=TRUE))
+
+#first 6 rows
+head(campylobacter_mutated)
+view(campylobacter_mutated)
+
+#creates a new variable in the campylobacter_mutated dataframe, difference (different between species mass and average mass for campy infection status)
+campylobacter_mutated$difference <- (campylobacter_mutated$mass - campylobacter_mutated$mean.mass.campy)
+
+
 
 
 
