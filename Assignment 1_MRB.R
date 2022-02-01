d2ab2ce5cbd1877fab11967711b46d0c48922e40
mrebrooks <mrebrooks@vt.edu>
2022-01-31 23:14:24 -0500

    Changed week 1 code to make it easier to run

--- Assignment 1_MRB.R
+++ Assignment 1_MRB.R
@@ -7,35 +7,35 @@ library(tidyverse)
 library(reshape2)
 
 #Prompt 1
-Campylobacter <- read.csv("campylobacterreview_data.csv") #reads csv file from my meta-analysis data into workspace
-view(Campylobacter) #calls up dataset into a separate window
+campylobacter <- read.csv("campylobacterreview_data.csv") #reads csv file from my meta-analysis data into workspace
+view(campylobacter) #calls up dataset into a separate window
 
 #Prompt 2
-Campylobacter[1] #calls first column
-Campylobacter[1,] #calls first row
-Campylobacter[1:6,] #calls first 6 rows
-summary(Campylobacter) #summary statistics
-head(Campylobacter) #calls first 6 values
+campylobacter[1] #calls first column
+campylobacter[1,] #calls first row
+campylobacter[1:6,] #calls first 6 rows
+summary(campylobacter) #summary statistics
+head(campylobacter) #calls first 6 values
 
 #adds campylobacter jejuni presence data and campylobacter coli presence data to create a new variable (CampySpecRich)
-Campylobacter$campyrichness <- (Campylobacter$cjejuni +
-  Campylobacter$ccoli)
+campylobacter$campyrichness <- (campylobacter$cjejuni +
+  campylobacter$ccoli)
 
 #prompt 3
 ?aggregate
 #aggregates data by C jejuni presence by C coli presence using the mean function
-m1 <- aggregate(cjejuni ~ ccoli,data=Campylobacter, FUN=mean)
+m1 <- aggregate(cjejuni ~ ccoli,data=campylobacter, FUN=mean)
 m1
 
 #aggregates data by C jejuni presence by C coli presence using the sum function
-m2 <- aggregate(cjejuni ~ ccoli,data=Campylobacter, FUN=sum)
+m2 <- aggregate(cjejuni ~ ccoli,data=campylobacter, FUN=sum)
 m2
 
 #aggregates data by C jejuni presence by C coli presence using the length function
-m3 <- aggregate(cjejuni ~ ccoli,data=Campylobacter, FUN=length)
+m3 <- aggregate(cjejuni ~ ccoli,data=campylobacter, FUN=length)
 m3
 
 #prompt 4
 #creates a data table with the variables 'Urban' and 'Campylobacter' from my dataset
-tab1 <- table(Campylobacter$urban, Campylobacter$campylobacter)
+tab1 <- table(campylobacter$urban, campylobacter$campy)
 tab1
