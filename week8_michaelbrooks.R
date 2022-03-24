b56599412d650ece7f1275c2c3e5f5d45a659061
mrebrooks <mrebrooks@vt.edu>
2022-03-24 16:03:05 -0400

    started second part

--- week8_michaelbrooks.R
+++ week8_michaelbrooks.R
@@ -1,12 +1,17 @@
 rm(list=ls())
 
+#PART 1
+
 #loads packages
 library(tidyverse)
 library(tidyr)
 library(ggplot2)
+library(emmeans)
+library(effects)
 
 #creates dataframe from csv file
 campy <- read.csv('campylobacterreview_data.csv')
+head(campy)
 
 #mass looks very right-skewed, not normal
 hist(campy$mass)
@@ -17,7 +22,8 @@ campy$logmass <- log(campy$mass)
 hist(campy$logmass)
 
 #I am going to create a univariate model testing the effect of trophic level (categorical) 
-#on mass (continuous) and I suspect that herbivores will generally be heavier based on the dataset
+#on mass (continuous) and I suspect that herbivores will generally be heavier than carnivores based on the dataset
+#Ha=logmass differs across trophic levels
 mod1=lm(logmass~trophic, data=campy)
 summary(mod1)
 plot(mod1)
@@ -34,8 +40,65 @@ shapiro.test(resid(mod1))
 #based on histogram and p value >0.05 on the shapiro-wilk test
 
 #diagnostic plot of logmass by trophic level
+#it certainly looks like herbivores and omnivores have a higher logmass than carnivores
 ggplot(campy, aes(x=trophic, y=logmass)) +
   geom_point()+
   stat_summary(color='red') +
   xlab('Trophic level') +
   ylab('Log of mass')
+
+
+
+
+
+
+
+#PART 2
+
+
+#Ha=logmass differs across trophic levels and sociality separately, but there are no interactions between the predictor variables
+#the intercept is social carnivore logmass
+#all other parameter estimates are comparing herbivore and omnivore logmass while keeping sociality constant
+mod2=lm(logmass~trophic+sociality, data=campy)
+summary(mod2)
+mod2
+
+plot(allEffects(mod2))
+
+
+#predict function
+
+pr <- with(campy,expand.grid(trophic=unique(trophic),sociality=unique(sociality)))
+pr
+pr$logmasspred <- predict(mod2,newdata=pr)
+pr
+
+
+
+
+ggplot(pp,aes(x=time,y=grahami,colour=light))+
+  geom_point()+
+  geom_line(aes(group=light))
+
+
+#Ha=logmass differs across trophic levels and sociality, and there is an interactive effect between these two predictors
+#the intercept is still social carnivore logmass
+#all other parameter estimates are comparing herbivore and omnivore logmass
+#also looks at the interactive effect between sociality and trophic level
+mod3=lm(logmass~trophic*sociality, data=campy)
+summary(mod3)
+mod3
+
+plot(allEffects(mod3))
+
+
+#this analysis reveals that when holding sociality constant, the only clear statistical difference
+#in logmass is between carnivores and herbivores
+lsm1<-emmeans(mod2,pairwise~trophic)
+lsm1
+pairs(lsm1)
+cld(lsm1$emmeans)
+
+
+
+
