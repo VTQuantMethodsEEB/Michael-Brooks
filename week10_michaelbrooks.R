e77246c6773dd88e48c024a6ee30d85ad36cd124
klangwig <klangwig@gmail.com>
2022-04-18 22:05:53 -0400

    sending assignment back to you

--- week10_michaelbrooks.R
+++ week10_michaelbrooks.R
@@ -18,15 +18,22 @@ pr <- function(m) printCoefmat(coef(summary(m)),
 
 #creates dataframe from csv file
 campy <- read.csv('campylobacterreview_data.csv')
+campy = campy %>%
+  drop_na(cjejuni)
+
 na.exclude(campy)
 head(campy)
 campy
+unique(campy$cjejuni)
+str(campy$cjejuni)
+campy$cjejuni = as.numeric(as.character(campy$cjejuni))
 
 #creates log of mass as a new variable
 campy$logmass <-log(campy$mass)
 
 #converts cjejuni to factor
-campy$cjejuni <- as.factor(campy$cjejuni)
+#campy$cjejuni <- as.factor(campy$cjejuni)
+#you don't want this as a factor!
 
 #plots logmass against cjejuni and groups by urban association via color
 plot1 <- campy %>%
@@ -43,6 +50,9 @@ plot1
 glm1 = glm(cjejuni~logmass+urban,data=campy, family="binomial") %>%
   tidy()%>%
   write_clip()
+glm1
+
+glm1 = glm(cjejuni~logmass+urban,data=campy, family="binomial") 
 #output indicates that urban dwellers are 
 #more likely to carry c jejuni than urban avoiders
 #mass (logmass in this case) does not appear to have an appreciable effect
@@ -58,31 +68,39 @@ summary(glm2)
 #as interacting with urban adaptation
 
 #creates new dataframe 
-campy.new=expand.grid(mass=seq(from = min(campy$logmass, na.rm=TRUE), to = max(campy$logmass, na.rm=TRUE), length.out = 100),
+campy.new=expand.grid(logmass=seq(from = min(campy$logmass, na.rm=TRUE), to = max(campy$logmass, na.rm=TRUE), length.out = 100),
                     urban = unique(campy$urban))
 campy.new
 
 #predicts output for category of input
-predict(glm1,newdata = campy.new)
+#predict(glm1,newdata = campy.new)
+
+
+#KL: i fixed this for you - it was logmass vs mass
 
 #creates new variable of predicted y aka yhat (probability of infection)
 campy.new$yhat  = predict(glm1,type="response",newdata = campy.new)
-campy$yhat2 = predict(glm1,type="response")
+#campy$yhat2 = predict(glm1,type="response")
 head(campy.new)
 campy.new
 
 #effects output for the binomial glm
 summary(allEffects(glm1))
-pr(summary(allEffects(glm1)))
+
+#not sure what the below is telling you - doesn't run
+#pr(summary(allEffects(glm1)))
 
 
 #creates second plot of cjejuni presence/absence as predicted by mass and urban association
-plot2=ggplot(data=campy,aes(x=mass,y=cjejuni,color=urban))+
+unique(campy$cjejuni)
+
+
+plot2=ggplot(data=campy,aes(x=logmass,y=cjejuni,color=urban))+
   geom_point(size=2,shape =1) +
-  geom_line(data=campy.new, aes(x=mass,y=yhat,col=urban))
+  geom_line(data=campy.new, aes(x=logmass,y=yhat,col=urban))
 plot2
 
-
+#much better! 
 
 
 #WEEK 11
@@ -105,17 +123,20 @@ campynaomit <-na.omit(campy)
 glm1=glm(cjejuni~urban, data=campynaomit, family="binomial")
 glm2=glm(cjejuni~urban + trophic, data=campynaomit, family="binomial")
 glm3=glm(cjejuni~urban + logmass, data=campynaomit, family="binomial")
+# models 2 and 3 are not nested - you can't compare them,
+# need to jave urban + trophic or urban interacting with tropic. can't add new vars. 
 glm4=glm(cjejuni~1, data=campynaomit, family="binomial")
 
 
+#you want to move from simple to complex
 #likelihood ratio test
-anova(glm1,glm2, test="LRT")
-anova(glm2, glm3, test="LRT")
-anova(glm1, glm3, test="LRT")
-anova(glm1, glm2, glm3, glm4, test="LRT")
+anova(glm4,glm1, test="LRT")
+anova(glm4, glm2, test="LRT")
+anova(glm4, glm3, test="LRT")
+anova(glm1, glm2, glm3, glm4, test="LRT")#KL: not all nested - shouldn't use!
 #based on the likelihood ratio test, no model appears to be "better" than any other
 #in predicting the probabililty of C. jejuni carriage
-
+#KL: 1 is better than 4, 2 is marginally better than 4, 3 is better than 4
 #AIC section
 AIC(glm1, glm2, glm3, glm4)
 #from this output, it appears that glm1 (c jejuni carriage as predicted by urban association)
