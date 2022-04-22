69ab17dbda980d077bcaa531877172e73ba410e6
mrebrooks <mrebrooks@vt.edu>
2022-04-22 16:27:31 -0400

    changed some code

--- finalproject_michaelbrooks.R
+++ finalproject_michaelbrooks.R
@@ -1,11 +1,29 @@
 rm(list=ls())
 
+#packages
 library(ggplot2)
 library(tidyverse)
 library(tidyr)
+library(ggplot2)
+library(emmeans)
+library(effects)
+library(MASS)
+library(broom)
+library(clipr)
+library(magrittr)
 
+#data
 campy <- read.csv('campylobacterreview_data.csv')
+campy
+head(campy)
 
-ggplot(data=campy, aes(x = urban, y = cjejuni, color=trophic))+
-  geom_point(aes(color=trophic)) +
-  geom_boxplot()
+#will be doing a GLM for this project
+#glm for cjejuni presence/absence as predicted by 
+#logmass and urban adaptation
+#my hypotheses are that urban dwellwers are more likely to carry c jejuni than avoiders
+#my second hypothesis is that animals with lower body mass are more likely to carry c jejuni
+#mass should be log transformed because otherwise it does not follow a normal distribution
+glm1 = glm(cjejuni~urban + trophic + sociality,data=campy, family="binomial")
+glm1
+summary(glm1)
+
